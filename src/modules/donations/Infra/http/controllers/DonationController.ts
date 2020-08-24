d2768fd2f0cf8247c2ed18f2d14137eaa850fe00
Gabriel Hauagge <gabrielhauagge@hotmail.com>
2020-08-24 20:54:15 -0300

    configuracao deploy

--- src/modules/donations/Infra/http/controllers/DonationController.ts
+++ src/modules/donations/Infra/http/controllers/DonationController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import { classToClass } from 'class-transformer';
-// import mercadopago from 'mercadopago';
+import mercadopago from 'mercadopago';
 
 import CreateDonationService from '@modules/donations/services/CreateDonationService';
 
@@ -11,16 +11,49 @@ export default class DonationController {
   public async create(request: Request, response: Response): Promise<any> {
     try {
       const { user_id } = request.params;
-      const { supplier_id, value } = request.body;
+      const { supplier_id, value, email } = request.body;
 
       const createDonation = container.resolve(CreateDonationService);
-      const donation = await createDonation.execute({
+
+      mercadopago.configure({
+        sandbox: true,
+        access_token: process.env.ACCESS_TOKEN_TEST,
+      });
+      // Create purchase item object template
+      const purchaseOrder = {
+        items: [
+          {
+            id: user_id,
+            title: 'donation',
+            description: 'donation',
+            quantity: 1,
+            currency_id: 'BRL',
+            unit_price: value,
+          },
+        ],
+        payer: {
+          email,
+        },
+        auto_return: 'all',
+        external_reference: user_id,
+        back_urls: {
+          success: `${process.env.APP_WEB_URL}/payments/success`,
+          pending: `${process.env.APP_WEB_URL}/payments/pending`,
+          failure: `${process.env.APP_WEB_URL}/payments/failure`,
+        },
+      };
+
+      const preference = await mercadopago.preferences.create(purchaseOrder);
+
+      console.log(preference);
+
+      await createDonation.execute({
         user_id,
         supplier_id,
         value,
       });
 
-      return response.json(classToClass(donation));
+      return response.redirect(`${preference.body.init_point}`);
     } catch (err) {
       return response.status(400).json({ error: err.message });
     }
